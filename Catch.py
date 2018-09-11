0316f517f040054a515b52ba2d9d410a801c5646
Konano <nanoapezlk@gmail.com>
2018-09-11 12:32:10 +0800

    debug OK

--- Catch.py
+++ Catch.py
@@ -2,7 +2,7 @@
 # @Author: NanoApe
 # @Date:   2018-09-11 00:47:45
 # @Last Modified by:   Konano
-# @Last Modified time: 2018-09-11 11:25:59
+# @Last Modified time: 2018-09-11 12:09:40
 
 import urllib.request
 import re
@@ -12,31 +12,33 @@ import codecs
 
 # import chardet
 
-maxlen = 100
+maxlen = 1000
 total = 0
+path = 'E:/tmp/htm/'
+path_fail = 'E:/tmp/htm_fail/'
+
+d = {}
 
 def getURLContent(url):
     print ("getURLContent: ", url)
-    while True:
-        flag = 1;
-        try:
-            headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
-            req = urllib.request.Request(url = url, headers = headers);
-            content = urllib.request.urlopen(req, timeout = 10).read();
-        except:
-            print ('get content Error:', url)
-            flag = 0;
-            time.sleep(5)
-        if flag == 1:
-            break;
+    headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+    req = urllib.request.Request(url = url, headers = headers)
+    content = urllib.request.urlopen(req, timeout = 10).read()
     return content.decode('utf-8','ignore')
 
-def store(content):
+def store(url,content):
     global total
-    total = total + 1
-    f = codecs.open(str(total)+'.htm','w','utf-8')
-    f.write(content)
-    f.close()
+    if content.find('<div id="p-detail">') != -1:
+        # 如果是新闻的话
+        total += 1
+        f = codecs.open(path+url.replace('/','.').replace(':',''),'w','utf-8')
+        f.write(content)
+        f.close()
+    else:
+        # 如果不是新闻的话
+        f = codecs.open(path_fail+url.replace('/','.').replace(':',''),'w','utf-8')
+        f.write(content)
+        f.close()
 
 def nextPage(content):
     p = re.compile(r'http://www.xinhuanet.com/[\/\-\w]+.htm')
@@ -44,11 +46,18 @@ def nextPage(content):
 
 if __name__ == '__main__':
     URL = ['http://www.xinhuanet.com/index.htm']
-    while URL:
+    while URL and total < 1000:
         print("Now queue length is ", len(URL))
-        content = getURLContent(URL[0])
+        if URL[0] in d:
+            del URL[0]
+            continue
+        try:
+            content = getURLContent(URL[0])
+            store(URL[0],content)
+            d[URL[0]] = True
+            if len(URL) < maxlen:
+                URL.extend(nextPage(content))
+        except:
+            print("Error!")
         del URL[0]
-        # if isNews(content):
-        store(content)
-        if len(URL) < maxlen:
-            URL.extend(nextPage(content))
+        time.sleep(0.5)
